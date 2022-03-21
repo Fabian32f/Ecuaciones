cb8dac049389160c31c020cbbe3d54626462cd2b
Fabian Adame <fabian.kinil@cbtis72.edu.mx>
2022-03-21 15:19:31 -0500

    Login1

--- app/src/main/java/com/cbtis/ecuaciones/EFormulaG.kt
+++ app/src/main/java/com/cbtis/ecuaciones/EFormulaG.kt
@@ -1,7 +1,12 @@
 package com.cbtis.ecuaciones
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class EFormulaG : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +16,108 @@ class EFormulaG : AppCompatActivity() {
 
 
 
+        val Calcular = findViewById<Button>(R.id.Re)
+
+
+
+        Calcular.setOnClickListener {
+
+            val a = findViewById<EditText>(R.id.Va )
+            val b = findViewById<EditText>(R.id.Vb )
+            val c = findViewById<EditText>(R.id.Vc )
+
+            val re = findViewById<TextView>(R.id.Tx1 )
+            val re2 = findViewById<TextView>(R.id.Tx2 )
+                var discriminante:Double
+                var x1:Double
+                var x2:Double
+
+
+                    /*
+                if((a.text.toString().toInt()) == 0){
+                    Toast.makeText(this,"No puede existir algun valor con cero para el valor a", Toast.LENGTH_SHORT).show()
+                }else if ((b.text.toString().toInt()) == 0){
+                    Toast.makeText(this,"No puede existir algun valor con cero para el valor b", Toast.LENGTH_SHORT).show()
+                }else if ((c.text.toString().toInt()) == 0){
+                    Toast.makeText(this,"No puede existir algun valor con cero para el valor c", Toast.LENGTH_SHORT).show()
+                }*/
+
+
+                //se calcula el discriminante del polinomio
+
+                discriminante = (b.text.toString().toDouble()*b.text.toString().toDouble())-(4*a.text.toString().toDouble()*c.text.toString().toDouble())
+                println("> Discriminante = $discriminante")
+
+                if (discriminante < 0) {
+
+                    Toast.makeText(this,"> Solución: Sin soluciones reales", Toast.LENGTH_SHORT).show()
+                    re.setText(
+                        "> Solución: Sin soluciones reales"
+
+                    )
+                    re2.setText(
+                        "> Solución: Sin soluciones reales"
+
+                    )
+
+
+                }else if (discriminante == 0.0) {
+
+
+                    x1 = (-b.text.toString().toDouble())/(2*a.text.toString().toDouble())
+
+                    Toast.makeText(this,"> Solucion: La ecuación tiene solamente una solución real: $x1", Toast.LENGTH_SHORT).show()
+                    re.setText(
+                        "X = "+x1.toString()
+
+                    )
+                    re2.setText(
+                        " "
+
+                    )
+
+
+                }else if (discriminante > 0) {
+
+                    Toast.makeText(this,"> Solución: ", Toast.LENGTH_SHORT).show()
+                    x1 = (-b.text.toString().toDouble()+Math.sqrt(discriminante))/(2*a.text.toString().toDouble())
+                    x2 = (-b.text.toString().toDouble()-Math.sqrt(discriminante))/(2*a.text.toString().toDouble())
+                    println("    X1 = $x1")
+                    println("    X2 = $x2")
+
+
+                    Toast.makeText(this,x1.toString(), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this,x2.toString(), Toast.LENGTH_SHORT).show()
+
+
+                    re.setText(
+                        "X1: "+x1.toString()
+
+                    )
+                    re2.setText(
+                        "X2: "+x2.toString()
+
+                    )
+
+
+                }
+
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
-}
--- app/src/main/java/com/cbtis/ecuaciones/ELineales.kt
+++ app/src/main/java/com/cbtis/ecuaciones/ELineales.kt
@@ -2,10 +2,79 @@ package com.cbtis.ecuaciones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class ELineales : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elineales)
+
+
+
+
+
+        val Calcular = findViewById<Button>(R.id.bCalcular)
+
+        Calcular.setOnClickListener {
+            val a1 = findViewById<EditText>(R.id.A1)
+            val b1 = findViewById<EditText>(R.id.B1)
+            val c1 = findViewById<EditText>(R.id.C1)
+            val a2 = findViewById<EditText>(R.id.A2)
+            val b2 = findViewById<EditText>(R.id.B2)
+            val c2 = findViewById<EditText>(R.id.C2)
+
+            val Rx = findViewById<TextView>(R.id.Tx )
+            val Ry = findViewById<TextView>(R.id.Ty )
+
+
+            val DS : Double  //Determinante del sistema
+            val Dx : Double //Determinante de X
+            val Dy : Double //Determinate de y
+            val X : Double  // valor de x
+            val Y : Double  //valor de y
+
+            DS = (a1.text.toString().toDouble()*b2.text.toString().toDouble())-(b1.text.toString().toDouble()*a2.text.toString().toDouble())
+            Toast.makeText(this,"Determinante del sistema: "+DS, Toast.LENGTH_SHORT).show()
+
+
+            Dx = (c1.text.toString().toDouble()*b2.text.toString().toDouble())-(b1.text.toString().toDouble()*c2.text.toString().toDouble())
+            Toast.makeText(this,"Determinante de X: "+Dx, Toast.LENGTH_SHORT).show()
+
+            Dy = (a1.text.toString().toDouble()*c2.text.toString().toDouble())-(c1.text.toString().toDouble()*a2.text.toString().toDouble())
+            Toast.makeText(this,"Determinante de Y: "+Dy, Toast.LENGTH_SHORT).show()
+
+            X = Dx/DS
+            Toast.makeText(this,"X = "+X, Toast.LENGTH_SHORT).show()
+            Y = Dy/DS
+            Toast.makeText(this,"Y = "+Y, Toast.LENGTH_SHORT).show()
+            Rx.setText(
+                "El valor de X es: "+X
+            )
+            Ry.setText(
+                "El valor de Y es: "+Y
+            )
+
+        }
+
+
+
+            /*
+        val n1 = findViewById<EditText>(R.id.n1 )
+        val n2 = findViewById<EditText>(R.id.n2 )
+        val r = findViewById<TextView>(R.id.res )
+
+        val bSumar = findViewById<Button>(R.id.boton)
+
+
+        bSumar.setOnClickListener {
+            r.setText(
+                "LA SUMA ES: "+(n1.text.toString().toInt()+n2.text.toString().toInt())
+            )
+
+        }*/
+
     }
 }
